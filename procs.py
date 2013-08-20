adba60b4735e3c4c5fb5a4a090f3bd4530758470
Tim Chen <chengt@gmail.com>
2013-08-20 15:31:27 -0500

    Monitoring support
    add way to monitor output
    add time to output

--- procs.py
+++ procs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import subprocess
+from datetime import datetime
 
 def execute(cmd):
     return subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True).communicate()[0];
@@ -9,12 +10,19 @@ def listRunning(proc, liveOnly=False):
     for task in subtasks.splitlines():
         stats = execute("cat /proc/%s/task/%s/stat" % (proc, task)).split()
         if stats[2] == 'R':
-            print "Subtask", stats[0], "is running on core", stats[len(stats) - 6]
+            print datetime.now(), "Subtask", stats[0], "is running on core", stats[len(stats) - 6]
         elif not liveOnly:
-            print "Subtask", stats[0], "is last run on core", stats[len(stats) - 6]
+            print datetime.now(), "Subtask", stats[0], "is last run on core", stats[len(stats) - 6]
 
 proc = raw_input("enter process id: ")
-liveOnly = raw_input("live [Y,N] (default N): ")
+
+liveOnly = raw_input("live only? [Y,N] (default N): ")
 liveOnly = True if liveOnly == 'Y' else liveOnly == 'y'
-print "Subtasks %s of Process %s: " % (liveOnly, proc)
-listRunning(proc, liveOnly)
+
+monitor = raw_input("monitor? [Y,N] (default N): ")
+monitor = True if monitor == 'Y' else monitor == 'y'
+
+keepGoing = True
+while keepGoing:
+    listRunning(proc, liveOnly)
+    keepGoing = monitor
